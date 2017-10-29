a305373f1ec5934405b11053ed816d0b52bc2976
abehnan <abehnan@gmail.com>
2017-10-29 15:37:45 -0400

    cleaned up welcome

--- echo.c
+++ echo.c
@@ -1,16 +1,19 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int main() {
-    char *data = getenv("QUERY_STRING");
+int main(void) {
 
+    char *data = getenv("QUERY_STRING");
     printf("Content-Type: text/html\n\n");
     printf("<html>");
     printf("<head>");
+    printf("<title>EROR</title>");
     printf("h2 {color: red;text-align: center;}");
     printf("</head>");
     printf("<body>");
-    printf("<h2>%s</h2>", &data);
+    printf("<h2>");
+    printf("%s", input);
+    printf("</h2>");
     printf("</body>");
     printf("</html>");
     return 0;
